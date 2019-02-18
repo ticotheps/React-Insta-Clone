e0fbd9e1a76db33725a55a3aa03edbd287521839
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-18 18:40:21 -0500

    Passed in props to <PostContainer /> component to give access to 'dummyData'

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,12 +2,12 @@ import React from 'react';
 import CommentSection from "../CommentSection/CommentSection";
 import "./PostContainer.css";
 
-const PostContainer = () => {
+const PostContainer = (props) => {
     return (
         <div className="post-container">
             <div className="post-header">
-                <img className="post-user-avatar" src="#" alt="User's avatar" />
-                <h2 className="post-user-name">Instagram Username</h2>
+                <img className="post-user-avatar" src={props.postContainer.thumbnailUrl} alt="User's avatar" />
+                <h2 className="post-user-name">{props.postContainer.username}</h2>
             </div>
             <div className="post-image-container">
                 <img className="post-image" src="#" alt=""/>
