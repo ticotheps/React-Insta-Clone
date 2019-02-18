9f2c1015efbc230512aa7dc77429788f0febef85
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-18 17:30:10 -0500

    Renamed 'dummy-data.js' file to 'dummyData.js' and imported that file into App.js file

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
+import dummyData from './dummyData';
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: dummyData
+    };
+  }
+
+
   render() {
     return (
       <div className="App">
