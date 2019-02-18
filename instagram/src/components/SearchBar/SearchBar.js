3d2a1152dc6468475bddae7dd1ee1bcbbb353a0b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-18 16:53:29 -0500

    Completed layout and styling for <SearchBar /> component and applied CSS Reset to index.css

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './SearchBar.css';
 
 const SearchBar = () => {
     return (
         <div className="search-bar">
-            <div>
-                <i class="fab fa-instagram"></i>
+            <div className="search-bar-container-left">
+                <i className="fab fa-instagram"></i>
+                <h2 className="search-bar-heading">Instagram</h2>
+            </div>
+            <div className="search-bar-container-middle">
+                <input className="search-bar-user-input" type="text" placeholder="Search"/>
+            </div>
+            <div className="search-bar-container-right">
+                <i className="far fa-compass"></i>
+                <i className="far fa-heart"></i>
+                <i className="far fa-user"></i>
             </div>
         </div>
     );
