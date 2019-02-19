e8589aac3d5c417cd1d84e541cba2e972b36c080
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-19 14:26:26 -0500

    Created a class component inside of <CommentSection /> component to extend React.Component and also created a new <CommentInput /> subcomponent

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import Comment from "./Comment";
-import "./CommentSection.css";
+import CommentInput from "./CommentInput";
+import "./Comment.css";
 
-const CommentSection = (props) => {
-    return (
-        <div className="comment-section">
-            <div className="comment-section-interaction">
-                <div className="comment-section-icons">
-                    <a href="https://instagram.com"><i className="far fa-heart"></i></a>
-                    <a href="https://instagram.com"><i class="far fa-comment"></i></a>
+
+class CommentSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: props.comments
+        };
+    }
+    render() {
+        return (
+            <div className="comment-section">
+                <div className="comment-section-interaction">
+                    <div className="comment-section-icons">
+                        <a href="https://instagram.com"><i className="far fa-heart"></i></a>
+                        <a href="https://instagram.com"><i class="far fa-comment"></i></a>
+                    </div>
+                    <div className="comment-section-likes">
+                        <p className="likes-display">373 likes</p>
+                    </div>
                 </div>
-                <div className="comment-section-likes">
-                    <p className="likes-display">373 likes</p>
+                <div className="comment-section-feed">
+                    <Comment />
                 </div>
+                <CommentInput />
             </div>
-            <div className="comment-section-feed">
-                <Comment />
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
+
 
 export default CommentSection;
