a033eff2bcf5fd216d62a23e05f5390f9ecba7ff
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-19 11:39:28 -0500

    Built out <CommentSection /> component and also created <Comment /> subcomponent with associated Comment.css file

---
+++ instagram/src/components/CommentSection/Comment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Comment.css";
+
+const Comment = () => {
+    return (
+        <div className="comment">
+            <h4>Hi. I'm a comment.</h4>
+        </div>
+    );
+};
+
+export default Comment;
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Comment from "./Comment";
 import "./CommentSection.css";
 
 const CommentSection = (props) => {
     return (
         <div className="comment-section">
-            <h2>Comments go here!</h2>
+            <div className="comment-section-interaction">
+                <div className="comment-section-icons">
+                    <a href="https://instagram.com"><i className="far fa-heart"></i></a>
+                    <a href="https://instagram.com"><i class="far fa-comment"></i></a>
+                </div>
+                <div className="comment-section-likes">
+                    <p className="likes-display">373 likes</p>
+                </div>
+            </div>
+            <div className="comment-section-feed">
+                <Comment />
+            </div>
         </div>
     );
 };
