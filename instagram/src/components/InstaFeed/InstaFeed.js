59c89a61986c9d5b3a4f3d33f0e5d84dccd4611d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-19 18:51:04 -0500

    Converted <InstaFeed /> functional component to class component

--- instagram/src/components/InstaFeed/InstaFeed.js
+++ instagram/src/components/InstaFeed/InstaFeed.js
@@ -2,19 +2,26 @@ import React from 'react';
 import PostContainer from '../PostContainer/PostContainer';
 import './InstaFeed.css';
 
-const InstaFeed = (props) => {
-    return (
-        <div className="insta-feed">
-            <div>
-                {props.posts.map(postContainer => (
-                    <div className="post-card">
-                        <PostContainer key={postContainer.timestamp} postContainer={postContainer} />
-                    </div>
-                ))}
-            </div> 
-        </div>
-    );
-};
-
+class InstaFeed extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: props.posts
+        };
+    }
+    render() {
+        return (
+            <div className="insta-feed">
+                <div>
+                    {this.state.posts.map(postContainer => (
+                        <div className="post-card">
+                            <PostContainer key={postContainer.timestamp} postContainer={postContainer} />
+                        </div>
+                    ))}
+                </div> 
+            </div>
+        );
+    }
+}
 
 export default InstaFeed;
