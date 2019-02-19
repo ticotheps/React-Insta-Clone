3442e7c6844f541d7e0a2d6156116fb7277f3632
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-19 18:12:47 -0500

    Checked for prop-types in <InstaFeed /> component with 'InstaFeed.propTypes'

--- instagram/src/components/InstaFeed/InstaFeed.js
+++ instagram/src/components/InstaFeed/InstaFeed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostContainer from '../PostContainer/PostContainer';
+import PropTypes from 'prop-types';
 import './InstaFeed.css';
 
 const InstaFeed = (props) => {
@@ -15,5 +16,22 @@ const InstaFeed = (props) => {
         </div>
     );
 };
+
+InstaFeed.propTypes = {
+    posts: PropTypes.arrayOf(
+        PropTypes.shape({
+            username: PropTypes.string.isRequired,
+            thumnailUrl: PropTypes.string,
+            likes: PropTypes.number,
+            timestamp: PropTypes.string,
+            comments: PropTypes.arrayOf(
+                PropTypes.shape({
+                    username: PropTypes.string.isRequired,
+                    text: PropTypes.string
+                })
+            )
+        })
+    )
+}
 
 export default InstaFeed;
