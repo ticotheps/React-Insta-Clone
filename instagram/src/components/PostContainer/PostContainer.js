7b8151c0c2e44e696bfd6b57882dbbe389b100f0
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-19 23:53:25 -0500

    Built a class for <PostContainer /> component

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,19 +2,27 @@ import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
 import "./PostContainer.css";
 
-const PostContainer = (props) => {
-    return (
-        <div className="post-container">
-            <div className="post-header">
-                <img className="post-user-avatar" src={props.postContainer.thumbnailUrl} alt="User's avatar" />
-                <h2 className="post-user-name">{props.postContainer.username}</h2>
+class PostContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            postContainer: props.postContainer
+        };
+    }
+    render() {
+        return (
+            <div className="post-container">
+                <div className="post-header">
+                    <img className="post-user-avatar" src={this.state.postContainer.thumbnailUrl} alt="User's avatar" />
+                    <h2 className="post-user-name">{this.state.postContainer.username}</h2>
+                </div>
+                <div className="post-image-container">
+                    <img className="post-image" src={this.state.postContainer.imageUrl} alt=""/>
+                </div>
+                <CommentSection commentSection={this.state.postContainer} />
             </div>
-            <div className="post-image-container">
-                <img className="post-image" src={props.postContainer.imageUrl} alt=""/>
-            </div>
-            <CommentSection commentSection={props.postContainer} />
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default PostContainer;
